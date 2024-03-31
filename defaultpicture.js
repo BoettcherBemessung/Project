a3a2a1bce9889c2ade83fbb3e46bbe3cc13f47f7
BöttcherBemessung <nick.boettcher@gmx.de>
2024-03-31 20:20:39 +0200

    commit

--- defaultpicture.js
+++ defaultpicture.js
@@ -432,7 +432,7 @@ function showdefault2() {
     var bsp_number = canvas.getContext('2d');
     bsp_number.font = '9px Arial';
     bsp_number.fillStyle = 'black';
-    bsp_number.fillText("bsp", 80 + 125 * bsp / hsp, 8);
+    bsp_number.fillText("bsp = 64", 70 + 125 * bsp / hsp, 8);
 
 
     //Maßketten für Schraubenabstände vertikal
@@ -442,57 +442,78 @@ function showdefault2() {
         var u1n_number = canvas.getContext('2d');
         u1n_number.font = '9px Arial';
         u1n_number.fillStyle = 'black';
-        u1n_number.fillText("u1n", 100 + 250 * bsp / hsp, 149 - 0.5 * u1n * y_factor);
+        u1n_number.fillText("u1n = 10", 100 + 250 * bsp / hsp, 149 - 0.5 * u1n * y_factor);
     }
 
     var gu_number = canvas.getContext('2d');
     gu_number.font = '9px Arial';
     gu_number.fillStyle = 'black';
-    gu_number.fillText("gu", 100 + 250 * bsp / hsp, 149 - (0.5 * gu + u1n) * y_factor);
+    gu_number.fillText("gu = 25", 100 + 250 * bsp / hsp, 149 - (0.5 * gu + u1n) * y_factor);
 
     var wh_number = canvas.getContext('2d');
     wh_number.font = '9px Arial';
     wh_number.fillStyle = 'black';
-    wh_number.fillText("w", 100 + 250 * bsp / hsp, 149 - (gu + u1n + 0.5 * (h - go - gu)) * y_factor);
+    wh_number.fillText("w = 67,6", 100 + 250 * bsp / hsp, 149 - (gu + u1n + 0.5 * (h - go - gu)) * y_factor);
 
     var go_number = canvas.getContext('2d');
     go_number.font = '9px Arial';
     go_number.fillStyle = 'black';
-    go_number.fillText("go", 100 + 250 * bsp / hsp, 149 - (u1n + h - 0.5 * go) * y_factor);
+    go_number.fillText("go = 25", 100 + 250 * bsp / hsp, 149 - (u1n + h - 0.5 * go) * y_factor);
 
 
     var eO_number = canvas.getContext('2d');
     eO_number.font = '9px Arial';
     eO_number.fillStyle = 'black';
-    eO_number.fillText("ex", 100 + 250 * bsp / hsp, 24 + 0.5 * ex * y_factor)
+    eO_number.fillText("ex = 15", 100 + 250 * bsp / hsp, 24 + 0.5 * ex * y_factor)
 
     var mvE_number = canvas.getContext('2d');
     mvE_number.font = '9px Arial';
     mvE_number.fillStyle = 'black';
-    mvE_number.fillText("mx", 100 + 250 * bsp / hsp, 24 + ex * y_factor + 0.5 * (u1 - ex) * y_factor)
+    mvE_number.fillText("mx = 17,4", 100 + 250 * bsp / hsp, 24 + ex * y_factor + 0.5 * (u1 - ex) * y_factor)
 
 
     var hsp_number = canvas.getContext('2d');
     hsp_number.font = '9px Arial';
     hsp_number.fillStyle = 'black';
-    hsp_number.fillText("hsp", 50, 149 - 0.5 * hsp * y_factor)
+    hsp_number.fillText("hsp=", 40, 139 - 0.5 * hsp * y_factor)
+
+    var hsp_number = canvas.getContext('2d');
+    hsp_number.font = '9px Arial';
+    hsp_number.fillStyle = 'black';
+    hsp_number.fillText("160", 40, 149 - 0.5 * hsp * y_factor)
 
     if (u1n !== 0) {
         var u1nleft_number = canvas.getContext('2d');
         u1nleft_number.font = '9px Arial';
         u1nleft_number.fillStyle = 'black';
-        u1nleft_number.fillText("u1n", 18, 149 - 0.5 * u1n * y_factor);
+        u1nleft_number.fillText("u1n", 14, 149 - 0.5 * u1n * y_factor);
     }
 
     var h_number = canvas.getContext('2d');
     h_number.font = '9px Arial';
     h_number.fillStyle = 'black';
-    h_number.fillText("h", 16, 149 - (u1n + 0.5 * h) * y_factor);
+    h_number.fillText("h=", 16, 149 - (u1n + 0.5 * h) * y_factor);
+
+    var h_number = canvas.getContext('2d');
+    h_number.font = '9px Arial';
+    h_number.fillStyle = 'black';
+    h_number.fillText("117,6", 10, 159 - (u1n + 0.5 * h) * y_factor);
+
 
     var u1_number_left = canvas.getContext('2d');
     u1_number_left.font = '9px Arial';
     u1_number_left.fillStyle = 'black';
-    u1_number_left.fillText("u1", 16, 149 - (u1n + h + 0.5 * u1) * y_factor);
+    u1_number_left.fillText("u1=", 16, 149 - (u1n + h + 0.5 * u1) * y_factor);
+
+    var u1_number_left = canvas.getContext('2d');
+    u1_number_left.font = '9px Arial';
+    u1_number_left.fillStyle = 'black';
+    u1_number_left.fillText("32,4", 16, 159 - (u1n + h + 0.5 * u1) * y_factor);
+
+    var profile = canvas.getContext('2d');
+    profile.font = '9px Arial';
+    profile.fillStyle = 'black';
+    profile.fillText("IPEa 120", 12, 10);
 
     var e_measures = canvas.getContext('2d');
     e_measures.beginPath();
