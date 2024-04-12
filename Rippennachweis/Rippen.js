355a8290f99d66259c566f22ed534fe45e2f4846
BöttcherBemessung <nick.boettcher@gmx.de>
2024-04-12 14:26:16 +0200

    commit

--- Rippennachweis/Rippen.js
+++ Rippennachweis/Rippen.js
@@ -8,10 +8,37 @@ function startCalc() {
 
     var Fed = parseFloat(document.getElementById('ValueVed').value);
 
+    var hw = h - 2 * tf
+
     if (selectedlocation == "typeA") {
+
+        var kf = 6
+        var m1 = b / tw
+        var m2 = 0.02 * (hw / tf) ** 2
+        var ly = ss + 2 * tf * (1 + (m1 + m2) ** 0.5)
+
+    }
+
+    if (selectedlocation == "typeB") {
+
+        var kf = 3.5
 
 
 
     }
+    if (selectedlocation == "typeC") {
+
+        var kf = 2 + 6 * ((ss + c) / hw)
+        if (kf > 6) {
+            kf = 6
+        }
+
+
+
+
+    }
+
+
+
 
 }
