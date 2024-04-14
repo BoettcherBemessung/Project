c7bc98a6ece402a5034784ec5ab899569b3fb935
BöttcherBemessung <nick.boettcher@gmx.de>
2024-04-14 13:19:44 +0200

    commit

--- Service/service.js
+++ Service/service.js
@@ -3,4 +3,16 @@ function addText() {
     var paragraph = document.createElement("p");
     paragraph.textContent = text;
     document.getElementById("output").appendChild(paragraph);
+
+    localStorage.setItem("savedText", text);
+
 }
+
+window.onload = function() {
+    var savedText = localStorage.getItem("savedText");
+    if (savedText) {
+        var paragraph = document.createElement("p");
+        paragraph.textContent = savedText;
+        document.getElementById("output").appendChild(paragraph);
+    }
+};
