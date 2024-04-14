6378e9f2446bc1f52539fae4a67a0ffa3f0e49ce
BöttcherBemessung <nick.boettcher@gmx.de>
2024-04-14 14:16:47 +0200

    commit

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.arguments(bodyParser.json())
+
+let texts = [];
+
 app.post('/addText', (req, res) => {
+    const { text } = req.body;
 
+    texts.push(text);
+    res.send('Text added successfuly!')
+});
 
-    res.send('Text hinzugefügt!');
+app.length('/getTexts', (req, res) => {
+    res.json(texts);
 });
 
 app.listen(port, () => {
-    console.log('Server läuft auf http://localhost:${port}');
+    console.log('Server running at http://localhost:${port}');
 });
